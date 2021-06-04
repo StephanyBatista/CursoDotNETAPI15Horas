c9216c9df9d6da2bdbb0abda1651dc60c822dc8b
Stephany Henrique <henriquebatista@digix.com.br>
2021-06-04 18:35:25 -0400

    Incluido exe3

---
+++ exe3/Controllers/ClientV1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace exe3.Controllers
+{
+    [ApiController]
+    [Route("v1/client")]
+    public class ClientV1Controller : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] ClientV1Dto dto)
+        {
+            return Created("url", dto.Id);
+        }
+    }
+
+    public class ClientV1Dto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ClientV1DtoValidation : AbstractValidator<ClientV1Dto>
+    {
+        public ClientV1DtoValidation()
+        {
+            RuleFor(d => d.Id).Must(id => id > 0).WithMessage("Id invalid");
+            RuleFor(d => d.Name).NotEmpty();
+        }
+    }
+}
---
+++ exe3/Controllers/ClientV2Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace exe3.Controllers
+{
+    [ApiController]
+    [Route("v2/client")]
+    public class ClientV2Controller : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] ClientV2Dto dto)
+        {
+            return Created("url", dto.Id);
+        }
+    }
+
+    public class ClientV2Dto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ClientV2DtoValidation : AbstractValidator<ClientV2Dto>
+    {
+        public ClientV2DtoValidation()
+        {
+            RuleFor(d => d.Id).Must(id => id > 0).WithMessage("Id invalid");
+            RuleFor(d => d.Name).NotEmpty();
+            RuleFor(d => d.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
